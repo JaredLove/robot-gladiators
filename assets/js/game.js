269ada68b2981be4a421948e7cdbca83ce664494
JaredLove <lovejared91@Yahoo.com>
2023-09-26 19:58:05 -0500

    added shop-input for less tying

--- assets/js/game.js
+++ assets/js/game.js
@@ -101,32 +101,26 @@ const fight = (enemy) => {
 
 const shop = () => {
      // ask player what they'd like to do
-        const shopOptionPrompt = window.prompt(
-            "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        let shopOptionPrompt = window.prompt(
+            "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
         );
-
+        // JavaScript function that converts strings to integers. parseInt
+        shopOptionPrompt = parseInt(shopOptionPrompt);
    // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
-        playerInfo.refillHealth();
-        break;
-        case "UPGRADE":
-        case "upgrade":
-        playerInfo.upgradeAttack();
-        break;
-        case "LEAVE":
-        case "leave":
-        window.alert("Leaving the store.");
-    
-        // do nothing, so function will end
-        break;
+        case 1:
+            playerInfo.refillHealth();
+            break;
+        case 2:
+            playerInfo.upgradeAttack();
+            break;
+        case 3:
+            window.alert("Leaving the store.");
+            break;
         default:
-        window.alert("You did not pick a valid option. Try again.");
-    
-        // call shop() again to force player to pick a valid option
-        shop();
-        break;
+            window.alert("You did not pick a valid option. Try again.");
+            shop();
+            break;
   }
 }
 
